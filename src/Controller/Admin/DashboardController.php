145654cc29cd6cec7ba03f181ad072ed0c48bdb8
Alban <alban.primault@gmail.com>
2025-09-25 13:31:31 +0200

    aa

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,12 +8,12 @@ use App\Entity\ModeDeJeu;
 use App\Entity\Personnage;
 use App\Entity\SetArtefact;
 use App\Entity\BonusSet;
+use App\Entity\StatistiquePersonnage;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\ImageSet;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -38,6 +38,7 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Modes de jeu', 'fas fa-gamepad', ModeDeJeu::class);
         yield MenuItem::linkToCrud('Builds', 'fas fa-hammer', Build::class);
         yield MenuItem::linkToCrud('Équipements', 'fas fa-shield-alt', Equipement::class);
+        yield MenuItem::linkToCrud('Statistiques de Personnage', 'fas fa-chart-line', StatistiquePersonnage::class);
 
         // AJOUTER CES DEUX LIGNES
         yield MenuItem::linkToCrud('Sets d\'Artefacts', 'fas fa-layer-group', SetArtefact::class);
